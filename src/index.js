342e37cd04073c3c2cb83e5cb35eb87a7f3bd070
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-11 19:51:29 -0300

    Removido arquivo index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
-import "./index.css";
 import GlobalStyle from "./style/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
