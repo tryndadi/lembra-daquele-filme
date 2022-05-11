c4ac53bcb9896924e26df42c84d31d97ac79a9fe
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-11 19:48:53 -0300

    style: Aplicado reset e definidas variaveis para cores principaisi

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
 import "./index.css";
+import GlobalStyle from "./style/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
