c9686037aeae62972fca6bf215caed74d1b07d5a
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-11 20:20:07 -0300

    feat/ Adicionado componente Theme para compartilhamento de thema padrão

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
 import GlobalStyle from "./styles/GlobalStyle";
+import Theme from "./styles/Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>
 );
