b4715197d1603917751072c1fecac6487e614953
Arthur Gonçalves <arthur.r.goncalves@gmail.com>
2022-05-12 11:08:47 -0300

    feat: estilização, estrutura inicial e yup

---
+++ src/pages/login/index.js
@@ -0,0 +1,80 @@
+import React from "react"
+import { FaEnvelope, FaKey } from 'react-icons/fa'
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from '@hookform/resolvers/yup'
+import InputComponent from '../../components/Input'
+import logo from '../../images/logo.png'
+import { ContainerGeral } from "./style" 
+
+const LoginPage = () => {
+
+    const schema = yup.object().shape({
+
+        email: yup.string().required('Campo obrigatório.').email('Digite um email válido.'),
+        password: yup.string().required('Campo obrigatório.')
+    })
+
+    const {register, handleSubmit, formState: {errors}} = useForm({resolver: yupResolver(schema)})
+
+    const onSubmit = (data) => {
+
+        console.log(data);
+    
+        //LÓGICA DA API
+    }
+
+    return (
+
+        <>
+            <ContainerGeral>
+
+                <a className="ancora-logo" href="/"> {/*LandingPage*/}
+
+                    <img src={logo} alt="Logo" />
+
+                </a>
+
+                <div className="cont-form">
+
+                    <h2>Conecte-se</h2>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        
+                        <InputComponent
+                            type='email'
+                            icon={FaEnvelope}
+                            placeholder='Email'
+                            register={register}
+                            name='email'
+                            error={errors.email && errors.email.message}
+                        />
+
+                        <InputComponent
+                            type='password'
+                            icon={FaKey}
+                            placeholder='Senha'
+                            register={register}
+                            name='password'
+                            error={errors.password && errors.password.message}
+                        />
+
+                        <button type="submit">Cadastrar</button>
+                    </form>
+
+                    <span className="mensagem">Não possui uma conta? 
+                        <a href="/"> {/*CAMINHO REGISTER PAGE =============*/}
+                            <strong>Cadastre-se</strong>
+                        </a>
+                    </span>
+                </div>
+
+                <span className="copyright">Kenzie Academy Brazil &copy; 2022</span>
+            </ContainerGeral>
+
+        </>
+
+    )
+}
+
+export default LoginPage
