970ec3f70eca7d96f2a0a8b680ffa445a859aaf3
Beatriz Munhoz <beatrizmunhozlima@gmail.com>
2022-05-12 11:27:53 -0300

    feat/ Landing Page: Texto e imagens com descrição da aplicação

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,13 +1,21 @@
 import ButtonToPage from "../../components/buttonToPage"
 import landingPage_1 from "../../assets/img/landingPage_1.svg"
+import TextLandingPage from "../../components/textLandingPage"
+import { Header } from "./styles"
 
 const LandingPage = () => {
  return (
+    <>
+    <Header>
+        <ButtonToPage route='/login'>Entrar</ButtonToPage>
+    </Header>    
     <div>
         <ButtonToPage route='/register'>Faça seu cadastro</ButtonToPage>
         <ButtonToPage route='/login'>Entrar</ButtonToPage>
         <img src={landingPage_1} alt="Ilustração" />
     </div>
+        <TextLandingPage/>
+    </>
     
  )
 }
