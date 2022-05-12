3d40db22204c7637169581ae691857f10cf6c22d
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-12 12:27:52 -0300

    fix/ Adicionadas importações do react

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,15 +1,15 @@
-import ButtonToPage from "../../components/buttonToPage"
-import landingPage_1 from "../../assets/img/landingPage_1.svg"
+import React from "react";
+import ButtonToPage from "../../components/buttonToPage";
+import landingPage_1 from "../../assets/img/landingPage_1.svg";
 
 const LandingPage = () => {
- return (
+  return (
     <div>
-        <ButtonToPage route='/register'>Faça seu cadastro</ButtonToPage>
-        <ButtonToPage route='/login'>Entrar</ButtonToPage>
-        <img src={landingPage_1} alt="Ilustração" />
+      <ButtonToPage route="/register">Faça seu cadastro</ButtonToPage>
+      <ButtonToPage route="/login">Entrar</ButtonToPage>
+      <img src={landingPage_1} alt="Ilustração" />
     </div>
-    
- )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
