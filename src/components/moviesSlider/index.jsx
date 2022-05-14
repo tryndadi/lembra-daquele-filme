a3e7c863e4733681576bba9c56fe50f9a253a2fe
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-14 09:32:29 -0300

    refactor/ Removido fragment desnecessário

--- src/components/moviesSlider/index.jsx
+++ src/components/moviesSlider/index.jsx
@@ -8,36 +8,31 @@ const MoviesSections = () => {
   const { mediasList } = useTMDBMedias();
 
   return (
-    <>
-      <div>
-        {mediasList &&
-          mediasList.map(
-            (medias, i) =>
-              medias && (
-                <div key={i}>
-                  <h1 style={{ color: "#FFF" }}>{medias.title}</h1>
-                  <CustomSlider {...(i === 0 ? customSettings : settings)}>
-                    {medias.items.map((media) => (
-                      <CustomCard
-                        key={media.id}
-                        custom={i === 0 ? true : false}
-                      >
-                        <img
-                          src={
-                            imagePathPrefix +
-                            (i !== 0 ? media.poster_path : media.backdrop_path)
-                          }
-                          alt={media.title}
-                          width="100%"
-                        />
-                      </CustomCard>
-                    ))}
-                  </CustomSlider>
-                </div>
-              )
-          )}
-      </div>
-    </>
+    <div>
+      {mediasList &&
+        mediasList.map(
+          (medias, i) =>
+            medias && (
+              <div key={i}>
+                <h1 style={{ color: "#FFF" }}>{medias.title}</h1>
+                <CustomSlider {...(i === 0 ? customSettings : settings)}>
+                  {medias.items.map((media) => (
+                    <CustomCard key={media.id} custom={i === 0 ? true : false}>
+                      <img
+                        src={
+                          imagePathPrefix +
+                          (i !== 0 ? media.poster_path : media.backdrop_path)
+                        }
+                        alt={media.title}
+                        width="100%"
+                      />
+                    </CustomCard>
+                  ))}
+                </CustomSlider>
+              </div>
+            )
+        )}
+    </div>
   );
 };
 
