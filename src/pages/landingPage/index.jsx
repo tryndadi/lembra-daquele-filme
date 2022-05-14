ab5cff35bc0f89f6291093e48ef43a28f5601a66
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-13 23:32:08 -0300

    fix/ Corrigida quebra da landing page

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ButtonToPage from "../../components/buttonToPage";
 import landingPage_1 from "../../assets/img/landingPage_1.svg";
-import logo from "../../assets/img/logo.svg";
+import logo from "../../assets/img/logoCompleta.svg";
 import TextLandingPage from "../../components/textLandingPage";
 import FooterLandingPage from "../../components/footerLandingPage";
-import { Container, Header, LogoContainer, TopPageContainer } from "./styles";
+
+import { Header, LogoContainer, TopPageContainer, Container } from "./styles";
 
 const LandingPage = () => {
   return (
@@ -15,16 +16,14 @@ const LandingPage = () => {
       <TopPageContainer>
         <div>
           <LogoContainer>
-            <img src={logo} alt="" />
-            <div>
-              <h2>Lembra</h2>
-              <h1>Daquele</h1>
-              <h2>Filme?</h2>
-            </div>
+            <img className="img_logo" src={logo} alt="" />
+            <h2>Sua biblioteca de filmes ...</h2>
           </LogoContainer>
 
           <ButtonToPage route="/register">Faça seu cadastro</ButtonToPage>
-          <ButtonToPage route="/login">Entrar</ButtonToPage>
+          <div className="div_button-login">
+            <ButtonToPage route="/login">Entrar</ButtonToPage>
+          </div>
         </div>
         <img src={landingPage_1} alt="Ilustração" />
       </TopPageContainer>
