2e81314412b7c70402521db525aca8739c6b600e
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-15 19:24:07 -0300

    fix/ Ajustada execução de trailler

--- src/Providers/TrailersProvider/index.jsx
+++ src/Providers/TrailersProvider/index.jsx
@@ -9,8 +9,6 @@ const TrailersProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const getTrailer = async (media) => {
-    setIsOpen(true);
-
     try {
       const videos = await tmdbAccess
         .get(`/movie/${media.id}/videos?language=pt-BR`)
