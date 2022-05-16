b2f13aec3044908f53f026ba2be2742a6357c05d
stefanyfelix <stefanyfelix.m@gmail.com>
2022-05-16 16:33:25 -0300

    feat: adicionando keyframe login e register

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { ProSidebar, SidebarHeader, SidebarContent, SidebarFooter, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
+import {
+  ProSidebar,
+  SidebarHeader,
+  SidebarContent,
+  SidebarFooter,
+  Menu,
+  MenuItem,
+  SubMenu,
+} from "react-pro-sidebar";
+
 import { tmdbAccess } from "../../services/api";
 import { getByGenre } from "../../services/tmdb";
 import { useTMDBMedias } from "../../Providers/MediasProvider";
 import { Link } from "react-router-dom";
 
-
-
 const Sidebar = () => {
+  const userData = localStorage.getItem("userData");
 
-  const userData = localStorage.getItem('userData');
-
-  const {name, email} = JSON.parse(userData);
+  const { name, email } = JSON.parse(userData);
 
   const [movieGenres, setMovieGenres] = useState([]);
   const { isLoading, getMedias } = useTMDBMedias();
@@ -29,51 +34,50 @@ const Sidebar = () => {
     getMedias(filteredMovies);
   };
 
-
   return (
-      <ProSidebar 
-        width='18vw'
-      >
-        <SidebarHeader>
-          <img src="" alt="Imagem do usuário" />
-          <h2>{name}</h2>
-          <span>{email}</span>
-        </SidebarHeader>
+    <ProSidebar width="18vw">
+      <SidebarHeader>
+        <img src="" alt="Imagem do usuário" />
+        <h2>{name}</h2>
+        <span>{email}</span>
+      </SidebarHeader>
 
-        <SidebarContent>
-          <Menu>
-            <SubMenu title='Media'>
-              <MenuItem>Filmes</MenuItem>
-              <MenuItem>Séries</MenuItem>
-            </SubMenu>
+      <SidebarContent>
+        <Menu>
+          <SubMenu title="Media">
+            <MenuItem>Filmes</MenuItem>
+            <MenuItem>Séries</MenuItem>
+          </SubMenu>
 
-            <SubMenu title='Categorias'>
-              {movieGenres.genres &&
-                movieGenres.genres.map((genre) => (
-                  <MenuItem key={genre.id} onClick={() => handleFilterClick(genre)}>
-                    {genre.name}
-                  </MenuItem>
+          <SubMenu title="Categorias">
+            {movieGenres.genres &&
+              movieGenres.genres.map((genre) => (
+                <MenuItem
+                  key={genre.id}
+                  onClick={() => handleFilterClick(genre)}
+                >
+                  {genre.name}
+                </MenuItem>
               ))}
-            </SubMenu>
+          </SubMenu>
 
-            <SubMenu title='Conta'>
-              <MenuItem>Biblioteca</MenuItem>
-              <MenuItem>Perfil</MenuItem>
-            </SubMenu>
-          </Menu>
-        </SidebarContent>
+          <SubMenu title="Conta">
+            <MenuItem>Biblioteca</MenuItem>
+            <MenuItem>Perfil</MenuItem>
+          </SubMenu>
+        </Menu>
+      </SidebarContent>
 
-        <SidebarFooter>
-          <Menu>
-            <MenuItem>
-              Sair
-              <Link to='/'/>
-            </MenuItem>
-          </Menu>
-        </SidebarFooter>
-      </ProSidebar>
-  )
+      <SidebarFooter>
+        <Menu>
+          <MenuItem>
+            Sair
+            <Link to="/" />
+          </MenuItem>
+        </Menu>
+      </SidebarFooter>
+    </ProSidebar>
+  );
 };
-
 
 export default Sidebar;
