bc1d48b9faf4011d1d6ad4207b4f2d297cce6b01
Gisela Mariano <gisela-mariano@gotmail.com>
2022-05-16 11:30:03 -0300

    feat/ Estrutura base da sidebar

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { ProSidebar, SidebarHeader, SidebarContent, SidebarFooter, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { tmdbAccess } from "../../services/api";
+import { getByGenre } from "../../services/tmdb";
+import { useTMDBMedias } from "../../Providers/MediasProvider";
+import { Link } from "react-router-dom";
+
+
+
+
+const Sidebar = () => {
+
+  const userData = localStorage.getItem('userData');
+
+  const {name, email} = JSON.parse(userData);
+
+  const [movieGenres, setMovieGenres] = useState([]);
+  const { isLoading, getMedias } = useTMDBMedias();
+
+  useEffect(() => {
+    tmdbAccess
+      .get("/genre/movie/list?&language=pt-BR")
+      .then((resp) => setMovieGenres(resp.data))
+      .catch((err) => console.log(`erro getGenres => ${err}`));
+  }, []);
+
+  const handleFilterClick = async (genre) => {
+    const filteredMovies = await getByGenre(genre);
+    getMedias(filteredMovies);
+  };
+
+
+  return (
+    <ProSidebar>
+      <SidebarHeader>
+        <img src="" alt="Imagem do usuário" />
+        <h2>{name}</h2>
+        <span>{email}</span>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <Menu>
+          <SubMenu title='Media'>
+            <MenuItem>Filmes</MenuItem>
+            <MenuItem>Séries</MenuItem>
+          </SubMenu>
+
+          <SubMenu title='Conta'>
+            <MenuItem>Biblioteca</MenuItem>
+            <MenuItem>Perfil</MenuItem>
+          </SubMenu>
+
+          <SubMenu title='Categorias'>
+            {movieGenres.genres &&
+              movieGenres.genres.map((genre) => (
+                <MenuItem key={genre.id} onClick={() => handleFilterClick(genre)}>
+                  {genre.name}
+                </MenuItem>
+            ))}
+          </SubMenu>
+        </Menu>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <Menu>
+          <MenuItem>
+            Sair
+            <Link to='/'/>
+          </MenuItem>
+        </Menu>
+      </SidebarFooter>
+    </ProSidebar>
+  )
+};
+
+
+export default Sidebar;
--- src/pages/dashboard/style.js
+++ src/pages/dashboard/style.js
@@ -6,6 +6,8 @@ export const StyleContainer = styled.div`
 
   background: ${(props) => props.theme.colors.background.gradient};
 
+  /* display: flex; */
+
   header {
     width: 100vw;
     height: 90px;
@@ -65,6 +67,9 @@ export const StyleContainer = styled.div`
 
   main {
     padding: 0 15px 15px 15px;
+
+    /* display: flex;
+    flex-direction: column; */
 
     nav {
       width: 100%;
