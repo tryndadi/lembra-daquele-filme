dfe13298b68dd620d4ad849f863a4dfeca044de0
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-17 14:30:43 -0300

    feat/ Removidos espaços desnecessários

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -20,36 +20,34 @@ import { useWishList } from "../../Providers/WishListProvider";
 import { useCollection } from "../../Providers/CollectionProvider";
 import { useHistory } from "react-router-dom";
 
-
-
-
 const Dashboard = () => {
-
   const [openSidebar, setOpenSidebar] = useState(false);
   const { isLoading } = useTMDBMedias();
   const { isLoggedIn } = useUser();
 
-  const history = useHistory()
+  const history = useHistory();
 
   const handleLogout = () => {
+    localStorage.removeItem("userData");
 
-    localStorage.removeItem('userData');
-
-    history.push('/')
-  }
-
+    history.push("/");
+  };
 
   return isLoggedIn ? (
     <StyleContainer>
       <aside>
-        <SidebarMUI openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} handleLogout={handleLogout}/>
+        <SidebarMUI
+          openSidebar={openSidebar}
+          setOpenSidebar={setOpenSidebar}
+          handleLogout={handleLogout}
+        />
       </aside>
 
       <div className="cont-geral-dashboard">
         <header>
           <div className="cont-header">
             <div className="menu" onClick={() => setOpenSidebar(true)}>
-              <HiMenu width={70}/>
+              <HiMenu width={70} />
 
               <span>Menu</span>
             </div>
