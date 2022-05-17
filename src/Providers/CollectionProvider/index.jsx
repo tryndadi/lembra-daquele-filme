b883dba8c861f78f5c36280a0f2c34294f3b004e
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-17 17:28:24 -0300

    Criada nova page para filmes assistidos

--- src/Providers/CollectionProvider/index.jsx
+++ src/Providers/CollectionProvider/index.jsx
@@ -1,36 +1,60 @@
 import React from "react";
 import { createContext, useState, useContext } from "react";
+import { getFromStorage } from "../../assets/js/utils";
 import { fakeApiAccess } from "../../services/api";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export const CollectionContext = createContext([]);
 
 const CollectionProvider = ({ children }) => {
-  const [userData, setUserData] = useState(
-    JSON.parse(localStorage.getItem("userData"))
-  );
+  const history = useHistory();
 
-  const addMovieToCollection = async (movieId) => {
+  const addMovieToCollection = async (movie) => {
+    const { id, accessToken } = getFromStorage("userData") || {};
+
     fakeApiAccess.defaults.headers.post[
       "Authorization"
-    ] = `Bearer ${userData.accessToken}`;
+    ] = `Bearer ${accessToken}`;
     await fakeApiAccess
       .post("/watched", {
-        userId: userData.id,
-        movieId: movieId,
+        userId: id,
+        movieId: movie.id,
+        image: movie.poster_path,
+        title: movie.title || movie.name,
+        average: movie.vote_average,
+        vote_count: movie.vote_count,
       })
       .then((response) => console.log(response))
       .catch((err) => console.log(err));
   };
 
-  const removeMovieFromCollection = (elementId) => {
+  const removeMovieFromCollection = ({ id }) => {
+    const { accessToken } = getFromStorage("userData") || {};
     fakeApiAccess
-      .delete(`/watched/${elementId}`, {
+      .delete(`/watched/${id}`, {
         headers: {
-          Authorization: `Bearer ${userData.acessToken} `,
+          Authorization: `Bearer ${accessToken} `,
         },
       })
       .then((response) => console.log(response))
       .catch((err) => console.log(err));
+  };
+
+  const getCollection = async () => {
+    const { id, accessToken } = getFromStorage("userData") || {};
+
+    if (!id || !accessToken) {
+      history.push("/login");
+      return;
+    }
+
+    fakeApiAccess.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${accessToken}`;
+
+    return await fakeApiAccess
+      .get(`/watched/?userId=${id}`)
+      .then(({ data }) => data);
   };
 
   return (
@@ -38,6 +62,7 @@ const CollectionProvider = ({ children }) => {
       value={{
         addMovieToCollection,
         removeMovieFromCollection,
+        getCollection,
       }}
     >
       {children}
