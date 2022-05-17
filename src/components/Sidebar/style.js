295103e2a254ca8c96d9086e0b9e94a0e838a969
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-05-16 21:30:51 -0300

    refactor/ Refatorado código de estilos da sidebar

--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -1,32 +1,23 @@
 import styled from "styled-components";
 
-
-
 export const ContainerSidebar = styled.div`
-
   width: 270px;
   height: 100vh;
-
   background: ${(props) => props.theme.colors.grey.ligth};
 
-  .divider{
+  .divider {
     width: 100%;
     height: 1px;
-
     margin: 8px 0;
-
     background: rgba(211, 174, 174, 1);
   }
-`
+`;
 
 export const section = {
-
-  fontSize: '0.9rem',
-
-  color: 'var(--grey-low)',
-}
+  fontSize: "0.9rem",
+  color: "var(--grey-low)",
+};
 
 export const category = {
-
   pl: 4,
-}
+};
