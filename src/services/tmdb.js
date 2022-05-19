f62613a8f990a2cf54feb52e2322b34d37405955
imrochamatheus <im.rochamatheus@gmail.com>
2022-05-19 11:15:52 -0300

    refactor/ Retirada exibição de categorias adicionais no dashboard

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -48,97 +48,97 @@ export default {
         items: await tmdbAccess.get("/trending/all/week?language=pt-BR"),
       },
 
-      {
-        keyWord: "action",
-        title: "Ação",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=28&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "comedy",
-        title: "Comédia",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=35&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "horror",
-        title: "Terror",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=27&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "romance",
-        title: "Romance",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=10749&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "documentary",
-        title: "Documentários",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=99&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "music",
-        title: "Música",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=10402&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "science fiction",
-        title: "Ficção científica",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=878&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "war",
-        title: "Guerra",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=10752&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "western",
-        title: "Faroeste",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=37&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "mistery",
-        title: "Mistério",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=9648&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "fantasy",
-        title: "Fantasia",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=14&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "family",
-        title: "Família",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=10751&language=pt-BR"
-        ),
-      },
-      {
-        keyWord: "adventure",
-        title: "Aventura",
-        items: await tmdbAccess.get(
-          "/discover/movie?with_genres=12&language=pt-BR"
-        ),
-      },
+      // {
+      //   keyWord: "action",
+      //   title: "Ação",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=28&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "comedy",
+      //   title: "Comédia",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=35&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "horror",
+      //   title: "Terror",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=27&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "romance",
+      //   title: "Romance",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=10749&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "documentary",
+      //   title: "Documentários",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=99&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "music",
+      //   title: "Música",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=10402&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "science fiction",
+      //   title: "Ficção científica",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=878&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "war",
+      //   title: "Guerra",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=10752&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "western",
+      //   title: "Faroeste",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=37&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "mistery",
+      //   title: "Mistério",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=9648&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "fantasy",
+      //   title: "Fantasia",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=14&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "family",
+      //   title: "Família",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=10751&language=pt-BR"
+      //   ),
+      // },
+      // {
+      //   keyWord: "adventure",
+      //   title: "Aventura",
+      //   items: await tmdbAccess.get(
+      //     "/discover/movie?with_genres=12&language=pt-BR"
+      //   ),
+      // },
     ];
   },
 };
